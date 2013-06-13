b9c56643ea80d35fbdf0824bf705d214f853f430
Edward Kim <eddotman@gmail.com>
2013-06-12 20:28:29 -0400

    runs properly

--- local_atomic_motif.py
+++ local_atomic_motif.py
@@ -4,10 +4,10 @@ from numpy import *
 
 class LAM:
 
-	def __init__(self, num_neighbours = 8):
+	def __init__(self, num_neighbours, atoms):
 		
 		self.num_atoms = 1 + num_neighbours;
-		self.atoms = array((0, 4))
+		self.atoms = atoms
 
 	#Quaternion
 	def rotation_matrix (self, axis,theta):
@@ -51,19 +51,18 @@ class LAM:
 		return (180.0/pi)*arccos(dot(v1, v2)/(linalg.norm(v1)*linalg.norm(v2)))
 
 	#computes vector from a point and origin
-	def compute_vector (self, x, y, z, o=(0,0,0)):
+	def compute_vector (self, (x, y, z), o=(0,0,0)):
 		return (x-o[0], y-o[1], z-o[2])
 		
 	#Orients the LAM at the origin by pinning 3 atoms
 	#Defaults set for WWW model a-Si
-	# 
 	def orient_LAM (self, cluster):
 
 		#translate cluster to origin
 		origin = cluster[0]
 		
 		#shift to origin
-		for y in arange(1, cluster_num):
+		for y in arange(1, self.num_atoms):
 			cluster[y][1] -= origin[1]
 			cluster[y][2] -= origin[2]
 			cluster[y][3] -= origin[3]
@@ -71,85 +70,82 @@ class LAM:
 		cluster[0] = [origin[0], 0, 0, 0]
 	
 		#spherical transformation
-		for y in arange(cluster_num):
-			cluster[y][1],cluster[y][2],cluster[y][3] = sph_coords(cluster[y, 1:4])
+		for y in arange(self.num_atoms):
+			cluster[y][1],cluster[y][2],cluster[y][3] = self.sph_coords(cluster[y, 1:4])
 		
 		#fix one of the atoms to the z-axis
 		theta_adjust = cluster[1][2]
 		
 		#transform back to cartesian
-		for y in arange(cluster_num):
-			cluster[y][1],cluster[y][2],cluster[y][3] = crt_coords(cluster[y, 1:4])
+		for y in arange(self.num_atoms):
+			cluster[y][1],cluster[y][2],cluster[y][3] = self.crt_coords(cluster[y, 1:4])
 			
 		#Find rotation axis using cross product
 		rot_axis = cross([0,0,1], cluster[1, 1:4])
 		
 		#Apply quaternion transform
-		for y in arange(cluster_num):
-			cluster[y][1],cluster[y][2],cluster[y][3] = dot(rotation_matrix(rot_axis,theta_adjust),cluster[y, 1:4])
+		for y in arange(self.num_atoms):
+			cluster[y][1],cluster[y][2],cluster[y][3] = dot(self.rotation_matrix(rot_axis,theta_adjust),cluster[y, 1:4])
 		
 		#spherical transformation
-		for y in arange(cluster_num):
-			cluster[y][1],cluster[y][2],cluster[y][3] = sph_coords(cluster[y, 1:4])
+		for y in arange(self.num_atoms):
+			cluster[y][1],cluster[y][2],cluster[y][3] = self.sph_coords(cluster[y, 1:4])
 		
 		#fix one atom to the xz plane
 		phi_adjust = cluster[2][3]
 		
 		#rotate cluster
-		for y in arange(cluster_num):
+		for y in arange(self.num_atoms):
 			cluster[y][3] -= phi_adjust
 		
 		#transform back to cartesian
-		for y in arange(cluster_num):
-			cluster[y][1],cluster[y][2],cluster[y][3] = crt_coords(cluster[y, 1:4])
+		for y in arange(self.num_atoms):
+			cluster[y][1],cluster[y][2],cluster[y][3] = self.crt_coords(cluster[y, 1:4])
 		
 		#save clusters information
 		self.atoms = cluster
 		
 	#Analyzes cluster
-	def analyze_cluster (cluster, cluster_num = 8):
-
-		#Compute dihedral atom
-		dih_atom = (cluster[5], cluster[6], cluster[7])
+	def analyze_LAM (self, cluster):
 		
 		#compute dihedral distance
-		dih_dist = (linalg.norm(cluster[5,1:4]), linalg.norm(cluster[6,1:4]), linalg.norm(cluster[7,1:4]))
+		#dih_dist = (linalg.norm(cluster[5,1:4]), linalg.norm(cluster[6,1:4]), linalg.norm(cluster[7,1:4]))
 		
 		#Compute dihedral angle in spherical
-		dih_angle = ((180/pi)*sph_coords(cluster[5,1:4])[2], (180/pi)*sph_coords(cluster[6,1:4])[2], (180/pi)*sph_coords(cluster[7,1:4])[2])
+		#dih_angle = ((180/pi)*self.sph_coords(cluster[5,1:4])[2], (180/pi)*self.sph_coords(cluster[6,1:4])[2], (180/pi)*self.sph_coords(cluster[7,1:4])[2])
 		
 		#Compute xz angle
-		xz_angle =  compute_angle(cluster[1, 1:4], cluster[0, 1:4], cluster[2, 1:4])
+		xz_angle =  self.compute_angle(cluster[1, 1:4], cluster[0, 1:4], cluster[2, 1:4])
 		
 		#compute atom-1 distance
 		z_dist =  cluster[1][3]
 		
 		#compute atom 2 distance
-		atom2_dists = linalg.norm(cluster[2][1:4])
+		atom2_dist = linalg.norm(cluster[2][1:4])
 		
 		#compute atom 4 distance
-		atom4_dists = linalg.norm(cluster[4][1:4])
+		atom4_dist = linalg.norm(cluster[4][1:4])
 		
 		#compute 1-2, 1-3, and 1-4 distances
-		atom1_dist = (linalg.norm(compute_vector(cluster[1][1], cluster[1][2], cluster[1][3], o=cluster[2,1:4])), linalg.norm(compute_vector(cluster[1][1], cluster[1][2], cluster[1][3], o=cluster[3,1:4])), linalg.norm(compute_vector(cluster[1][1], cluster[1][2], cluster[1][3], o=cluster[4,1:4])))
+		#atom1_dist = (linalg.norm(self.compute_vector(cluster[1,1:4], o=cluster[2,1:4])), linalg.norm(self.compute_vector(cluster[1,1:4], o=cluster[3,1:4])), linalg.norm(self.compute_vector(cluster[1,1:4], o=cluster[4,1:4])))
 		
 		#compute 2-567, 3-567, and 4-567 distances
-		atom234_dist = (linalg.norm(compute_vector(cluster[2][1], cluster[2][2], cluster[2][3], o=cluster[5,1:4])), linalg.norm(compute_vector(cluster[2][1], cluster[2][2], cluster[2][3], o=cluster[6,1:4])), linalg.norm(compute_vector(cluster[2][1], cluster[2][2], cluster[2][3], o=cluster[7,1:4])), linalg.norm(compute_vector(cluster[3][1], cluster[3][2], cluster[3][3], o=cluster[5,1:4])), linalg.norm(compute_vector(cluster[3][1], cluster[3][2], cluster[3][3], o=cluster[6,1:4])), linalg.norm(compute_vector(cluster[3][1], cluster[3][2], cluster[3][3], o=cluster[7,1:4])), linalg.norm(compute_vector(cluster[4][1], cluster[4][2], cluster[4][3], o=cluster[5,1:4])), linalg.norm(compute_vector(cluster[4][1], cluster[4][2], cluster[4][3], o=cluster[6,1:4])), linalg.norm(compute_vector(cluster[4][1], cluster[4][2], cluster[4][3], o=cluster[7,1:4])))
+		#atom234_dist = (linalg.norm(self.compute_vector(cluster[2,1:4], o=cluster[5,1:4])), linalg.norm(self.compute_vector(cluster[2,1:4], o=cluster[6,1:4])), linalg.norm(self.compute_vector(cluster[2,1:4], o=cluster[7,1:4])), linalg.norm(self.compute_vector(cluster[3,1:4], o=cluster[5,1:4])), linalg.norm(self.compute_vector(cluster[3,1:4], o=cluster[6,1:4])), linalg.norm(self.compute_vector(cluster[3,1:4], o=cluster[7,1:4])), linalg.norm(self.compute_vector(cluster[4,1:4], o=cluster[5,1:4])), linalg.norm(self.compute_vector(cluster[4,1:4], o=cluster[6,1:4])), linalg.norm(self.compute_vector(cluster[4,1:4], o=cluster[7,1:4])))
 		
 		#compute the plane normal angle
-		basis1a = compute_vector(cluster[2][1], cluster[2][2], cluster[2][3], o=cluster[3, 1:4])
-		basis1b = compute_vector(cluster[4][1], cluster[4][2], cluster[4][3], o=cluster[3, 1:4])
+		basis1a = self.compute_vector(cluster[2,1:4], o=cluster[3, 1:4])
+		basis1b = self.compute_vector(cluster[4,1:4], o=cluster[3, 1:4])
 		axis1 = cross(basis1a, basis1b)
 		
-		basis2a = compute_vector(cluster[5][1], cluster[5][2], cluster[5][3], o=cluster[6, 1:4])
-		basis2b = compute_vector(cluster[7][1], cluster[7][2], cluster[7][3], o=cluster[6, 1:4])
+		basis2a = self.compute_vector(cluster[5,1:4], o=cluster[6, 1:4])
+		basis2b = self.compute_vector(cluster[7,1:4], o=cluster[6, 1:4])
 		axis2 = cross(basis2a, basis2b)
 		
-		axis_angle = compute_angle(axis1, (0,0,0), axis2)
+		axis_angle = self.compute_angle(axis1, (0,0,0), axis2)
 		
 		if axis_angle > 90.0:
 			axis_angle = 180.0 - axis_angle
 		
 		plane_angle = axis_angle
 			
-		return (dih_atom, dih_angle, xz_angle, z_dist, atom1_dist, atom2_dist, atom4_dist, atom234_dist, plane_angle, dih_dist)
+		return (xz_angle, z_dist, atom2_dist, atom4_dist, plane_angle)
--- local_atomic_motif_isolator.py
+++ local_atomic_motif_isolator.py
@@ -1,7 +1,5 @@
 #Written by Edward Kim
-
-#Script for plotting clusters of atoms
-#Intended to work with output from RMC++
+#Script for plotting local atomic motifs of atoms
 
 #Import necessary libraries
 from numpy import *
@@ -11,30 +9,33 @@ from periodic_kdtree import PeriodicCKDTree
 
 class LAMisolator(): 
 
-	def __init__():
-		self.LAMs = empty()
+	def __init__(self):
+		self.LAMs = empty(0)
+		self.LAMatoms = empty((0,4))
 
 	#Imports a text (data) file
-	def load_file (file_name, start_row = 0):
+	def load_file (self, file_name, start_row = 0):
 		return loadtxt(file_name, skiprows = start_row)
 		
 	#Saves a files
-	def save_file (location, file, format):
-		savetxt(location, file, fmt=format)
+	def save_file (self, location, data):
+		savetxt(location, data)
 		
 	#Saves nearest neighbours
-	def compute_nn_list (file_name, start_row, num_nn):
-		f1 = load_file(file_name, start_row)
+	def compute_nn_list (self, file_name, start_row, num_nn):
+		f1 = self.load_file(file_name, start_row)
 		bounds = array([1.999999999, 1.999999999, 1.999999999]) #Can't do exactly 2.0 because rounding
 		atom_list = PeriodicCKDTree(bounds, f1)
 		nn = atom_list.query(f1, k=num_nn)
-		save_file("nn_list", nn[1], "%.6d")
+		self.save_file(file_name + "_nn", nn[1], "%.6d")
+
+		print "Nearest-neighbour list successfully computed!"
 
 	#Default settings are for a-Si WWW model
-	def plot_LAMs(cfg_file, cfg_start_row, num_atoms, cluster_coord=4, nn_file = "nn_list", box_length=62.024362):
+	def read_LAMs(self, cfg_file, cfg_start_row, num_atoms, cluster_coord, nn_file, box_length=62.024362):
 
 		#Load file
-		f_cfg = load_file(cfg_file, cfg_start_row)
+		f_cfg = self.load_file(cfg_file, cfg_start_row)
 		
 		#Resize box and reorient
 		f_cfg = dot(box_length, f_cfg) 
@@ -42,15 +43,15 @@ class LAMisolator():
 		f_cfg = transpose(f_cfg)
 
 		#Plot clusters within model using NNs
-		f_nn = load_file(nn_file)
+		f_nn = self.load_file(nn_file)
 
-		#Holds cluster coords
-		clusters = empty((0,4))
+		#Holds LAMs
+		LAMs = empty(0)
 
-		#Plot single-clusters
+		#Plot LAMs
 		for x in arange(f_nn.shape[0]):
 			
-			cluster = empty((cluster_coord + 1,4))
+			cluster = empty((cluster_coord + 1, 4))
 			
 			for y in arange(cluster_coord + 1):
 				cluster[y] = f_cfg[f_nn[x][y]]
@@ -61,18 +62,59 @@ class LAMisolator():
 			if plot_aniso is True:
 				#Look for NNs of the NN-1 (which will be fixed to z-axis)
 				nn_list = f_nn[cluster[1][0]]
+				
 				for y in arange(1, 2*cluster_coord):
 					if (nn_list[y] != cluster[0][0]) and (nn_list[y] != cluster[1][0]) and (nn_list[y] != cluster[2][0]) and (nn_list[y] != cluster[3][0]) and (nn_list[y] != cluster[4][0]) and (cluster.shape[0] < 2*cluster_coord):
 						cluster = vstack((cluster, f_cfg[nn_list[y]]))
 								
-					#number of atoms in cluster
-					cluster_num = 2 * cluster_coord
-				else:
-					cluster_num = cluster_coord + 1
+				#number of atoms in cluster
+				cluster_num = 2 * cluster_coord
+			else:
+				cluster_num = cluster_coord + 1
 					
-				#check for PBC
-				L = box_length #half-dimension of box
-				for y in arange(1, cluster_num):
-					for z in arange(1, 4):
-						if abs(cluster[y][z]-cluster[0][z]) > L:
-							cluster[y][z] -= sign(cluster[y][z]-cluster[0][z])*L*2
+			#check for PBC
+			L = box_length #half-dimension of box
+			for y in arange(1, cluster_num):
+				for z in arange(1, 4):
+					if abs(cluster[y][z]-cluster[0][z]) > L:
+						cluster[y][z] -= sign(cluster[y][z]-cluster[0][z])*L*2
+
+			#Store LAM
+			LAM_temp = LAM(cluster_coord, cluster)
+			LAMs = append(LAMs, LAM_temp)
+			self.LAMatoms = append(self.LAMatoms, cluster, axis=0)
+
+		self.LAMs = LAMs
+		print "LAMs successfully read from file!"
+
+	def orient_LAMs (self):
+		for LAM in self.LAMs:
+			LAM = LAM.orient_LAM(LAM.atoms)
+		print "LAMs successfully oriented!"
+
+	def save_LAMs (self, savefile):
+		self.save_file(savefile + "_LAMs", self.LAMatoms)
+
+		print "LAMs successfully saved!"
+
+	def save_LAMdata (self, savefile):
+		LAMdata = empty((0,5))
+
+		for LAM in self.LAMs:
+			LAMdata = vstack((LAMdata, LAM.analyze_LAM(LAM.atoms)))
+
+		self.save_file(savefile + "_LAMdata", LAMdata)
+
+		print "LAM data successfully analyzed and saved!"
+
+
+if __name__ == "__main__":
+	
+	iso1 = LAMisolator()
+
+	#iso1.compute_nn_list("www_coords", 0, 18)
+
+	iso1.read_LAMs("www_coords", 0, 100000, 16, "www_coords_nn")
+	iso1.orient_LAMs()
+	#iso1.save_LAMdata("www_coords")
+	iso1.save_LAMs("www_coords")
